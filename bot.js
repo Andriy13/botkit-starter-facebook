1d32ffd927a7025d2535a009436b9bd6dce9047e
Andriy13 <andriylomaga@gmail.com>
2018-12-03 19:53:40 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -77,56 +77,6 @@ var bot = controller.spawn({
 });
 
 
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (process.env.studio_token) {
-    controller.on('message_received,facebook_postback', function(bot, message) {
-        if (message.text) {
-            controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
-                if (!convo) {
-                    // no trigger was matched
-                    // If you want your bot to respond to every message,
-                    // define a 'fallback' script in Botkit Studio
-                    // and uncomment the line below.
-                    controller.studio.run(bot, 'fallback', message.user, message.channel, message);
-                } else {
-                    // set variables here that are needed for EVERY script
-                    // use controller.studio.before('script') to set variables specific to a script
-                    convo.setVar('current_time', new Date());
-                }
-            }).catch(function(err) {
-                if (err) {
-                    bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-                    debug('Botkit Studio: ', err);
-                }
-            });
-        }
-    });
-} else {
-    console.log('~~~~~~~~~~');
-    console.log('NOTE: Botkit Studio functionality has not been enabled');
-    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-}
-
-function usage_tip() {
-    console.log('~~~~~~~~~~');
-    console.log('Botkit Studio Starter Kit');
-    console.log('Execute your bot application like this:');
-    console.log('page_token=<MY PAGE TOKEN> verify_token=<MY VERIFY TOKEN> studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
-    console.log('Get Facebook token here: https://developers.facebook.com/docs/messenger-platform/implementation')
-    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
-    console.log('~~~~~~~~~~');
-}
-
-controller.hears(['hello'],'direct_mention, direct_message', function(bot, message) {
-
-    bot.reply(message,'Howdy!');
-  
-});
 
 controller.on('facebook_postback', function(bot, message) {
     if(message.payload ==='catalogue')
@@ -139,7 +89,7 @@ controller.on('facebook_postback', function(bot, message) {
     
     if(message.payload ==='main_menu'||message.payload ==='get_started_payload')
     {
-        bot.startConversation(message, function(err, convo) {
+        /*bot.startConversation(message, function(err, convo) {
             convo.ask({
                     "text": "Main menu",
                     "quick_replies": [
@@ -168,9 +118,37 @@ controller.on('facebook_postback', function(bot, message) {
                 convo.say("Function "+response.text+" is still yet to be added");
                 convo.next();
             });
+        });*/
+        bot.reply(message,{
+            "text": "Main menu",
+            "quick_replies": [
+                {
+                    "content_type": "text",
+                    "title": "My purchases",
+                    "payload": "menu_purchases"
+                },
+                {
+                    "content_type": "text",
+                    "title": "Shop",
+                    "payload": "menu_shop"
+                },
+                {
+                    "content_type": "text",
+                    "title": "Favorites",
+                    "payload": "menu_favs"
+                },
+                {
+                    "content_type": "text",
+                    "title": "To invite a friend",
+                    "payload": "menu_invite"
+                }
+            ]
         });
     }
-    
+    switch(message.payload)
+    {
+        case 'menu_shop': bot.reply(message,'no shop');
+    }
 });
 
 
